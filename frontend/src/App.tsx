e968f3baf50cf9757e885a72edcbcc5443fee02b
Jonas Honecker <honeckerjonas@gmail.com>
2024-05-30 10:26:53 +0200

    Implemented GET all endpoint on reload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,47 +3,41 @@ import KanBanBoard from "./components/KanBanBoard.tsx";
 import KanBanCard from "./components/KanBanCard.tsx";
 import KanBanItem from "./components/KanBanItem.tsx";
 import {KanBanItemProps} from "./types/KanBanItemProps.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-    const itemsFromAPI: KanBanItemProps[] = [
-        {
-            id: "1",
-            status: "OPEN",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore."
-        },
-        {
-            id: "2",
-            status: "IN_PROGRESS",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore."
-        },
-        {
-            id: "3",
-            status: "DONE",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore."
-        },
-        {
-            id: "4",
-            status: "DONE",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing."
-        },
-        {
-            id: "5",
-            status: "DONE",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod  tempor incididunt ut labore et dolore."
-        }
-    ]
 
-    const [items, setItems] = useState(itemsFromAPI)
+    const [items, setItems] = useState<KanBanItemProps[]>([])
 
-    const openItems = items.filter((item) => {return item.status === "OPEN"})
-        .map((item) => {return <KanBanItem key={item.id} kanBanItemProps={item} items={items} setItems={setItems}/>})
+    const loadItems = () => {
+        axios.get<KanBanItemProps[]>("api/todo").then((response) => {
+            setItems(response.data);
+        })
+    }
 
-    const inProgressItems = items.filter((item) => {return item.status === "IN_PROGRESS"})
-        .map((item) => {return <KanBanItem key={item.id} kanBanItemProps={item} items={items} setItems={setItems}/>})
+    useEffect(() => {loadItems()}, [])
 
-    const doneItems = items.filter((item) => {return item.status === "DONE"})
-        .map((item) => {return <KanBanItem key={item.id} kanBanItemProps={item} items={items} setItems={setItems}/>})
+    const openItems = items.filter((item) => {
+        return item.status === "OPEN"
+    })
+        .map((item) => {
+            return <KanBanItem key={item.id} kanBanItemProps={item} items={items} setItems={setItems}/>
+        })
+
+    const inProgressItems = items.filter((item) => {
+        return item.status === "IN_PROGRESS"
+    })
+        .map((item) => {
+            return <KanBanItem key={item.id} kanBanItemProps={item} items={items} setItems={setItems}/>
+        })
+
+    const doneItems = items.filter((item) => {
+        return item.status === "DONE"
+    })
+        .map((item) => {
+            return <KanBanItem key={item.id} kanBanItemProps={item} items={items} setItems={setItems}/>
+        })
 
     return (
         <KanBanBoard>
